7d814f8be021b4b0e0b4c9c835b27b1069e59d3e
gelhteag <45104871+gelhteag@users.noreply.github.com>
2018-11-26 17:18:55 +0100

    Add files via upload

---
+++ EcamusGame/src/main/java/main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Screen s = new Screen();
+
+        JFrame window =s.window;
+
+
+        //Arene.startFight(Arene.listPersonnages.get(0) , Arene.listPersonnages.get(1)); // on lance le combat
+
+    }
+}
